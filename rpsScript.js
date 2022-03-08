75624a8611e9673397a74c3aac7d5abf89cd285c
Brian <technophile1974@gmail.com>
2022-03-08 04:23:18 -0500

    added a function that returns the result of the round

--- rpsScript.js
+++ rpsScript.js
@@ -1,6 +1,41 @@
 function computerPlay(){
     const choices = ["Rock", "Paper", "Scissors"];
     const random = choices[Math.floor(Math.random() * choices.length)];
-    console.log(random);
+    return random;
 }
 
+let info = window.prompt("Choose Rock, Paper, or Scissors");
+playerSelection = info.charAt(0).toUpperCase() + info.slice(1).toLowerCase();
+computerSelection = computerPlay();
+function playRound(playerSelection, computerSelection){
+    if(computerSelection == playerSelection){
+        return "It's a tie!";
+    }
+    if(computerSelection == "Rock" && playerSelection == "Paper"){
+        return alert("I chose Rock. You chose Paper. Paper covers Rock. You win!");
+    }
+    if(computerSelection == "Rock" && playerSelection == "Scissors"){
+        return "Rock breaks Scissors. I win!"
+    }
+    if(computerSelection == "Paper" && playerSelection == "Scissors"){
+        return "Scissors cut Paper. You win!";
+    }
+    if(computerSelection == "Paper" && playerSelection == "Rock"){
+        return "Paper covers Rock. I win!";
+    }
+    if(computerSelection == "Scissors" && playerSelection == "Rock"){
+        return "Rock breaks Scissors. You win!";
+    }
+    if(computerSelection == "Scissors" && playerSelection == "Paper"){
+        return "Scissors cut Paper. I win!";
+    }
+}
+
+
+console.log(`You chose ${playerSelection}`);
+console.log(`I chose ${computerSelection}`);
+console.log(playRound(playerSelection, computerSelection));
+
+function game(){
+  
+}
