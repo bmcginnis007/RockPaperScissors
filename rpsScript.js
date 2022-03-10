6b4b7d6012223917030510787751a03862120497
Brian <technophile1974@gmail.com>
2022-03-10 06:40:57 -0500

    added scoring and winner display message

--- rpsScript.js
+++ rpsScript.js
@@ -4,6 +4,13 @@ function computerPlay(){
     return random;
 }
 
+alert("Welcome to Rock Paper Scissors");
+alert("We will play 5 rounds in a game. Best of 5 wins. Good luck!");
+
+let compScore = 0;
+let playerScore = 0;
+
+
 let info = window.prompt("Choose Rock, Paper, or Scissors");
 playerSelection = info.charAt(0).toUpperCase() + info.slice(1).toLowerCase();
 computerSelection = computerPlay();
@@ -12,30 +19,64 @@ function playRound(playerSelection, computerSelection){
         return alert("It's a tie!");
     }
     if(computerSelection == "Rock" && playerSelection == "Paper"){
-        return alert("I chose Rock. You chose Paper. Paper covers Rock. You win!");
+        ++playerScore
+        return alert("I chose Rock. You chose Paper. Paper covers Rock. You have " + `${playerScore}` + " point."); 
     }
     if(computerSelection == "Rock" && playerSelection == "Scissors"){
-        return alert("Rock breaks Scissors. I win!");
+        ++compScore
+        return alert("I chose Rock. You chose Scissors. Rock breaks Scissors. I have " + `${compScore}` + " point.");
     }
     if(computerSelection == "Paper" && playerSelection == "Scissors"){
-        return alert("Scissors cut Paper. You win!");
+        ++playerScore
+        return alert("I chose Paper. You chose Scissors. Scissors cut Paper. You have " + `${playerScore}` + " point.");
     }
     if(computerSelection == "Paper" && playerSelection == "Rock"){
-        return alert("Paper covers Rock. I win!");
+        ++compScore
+        return alert("I chose Paper. You chose Rock. Paper covers Rock. I have " +`${compScore}` + " point.");
     }
     if(computerSelection == "Scissors" && playerSelection == "Rock"){
-        return alert("Rock breaks Scissors. You win!");
+        ++playerScore
+        return alert("I chose Scissors. You chose Rock. Rock breaks Scissors. You have " +`${playerScore}` + " point.");
     }
     if(computerSelection == "Scissors" && playerSelection == "Paper"){
-        return alert("Scissors cut Paper. I win!");
+        ++compScore
+        return alert("I chose Scissors. You chose Paper. Scissors cut Paper. I have " +`${compScore}` + " point.");
     }
 }
 
-location.reload()
+
 
 
 console.log(`You chose ${playerSelection}`);
 console.log(`I chose ${computerSelection}`);
 console.log(playRound(playerSelection, computerSelection));
 
-// loops playRound 5 times and declares a winner
+function game(){
+    for (let i = 0; i < 5; i++) {
+        if (i < 4) {
+            let info = window.prompt("Choose Rock, Paper, or Scissors");
+            playerSelection = info.charAt(0).toUpperCase() + info.slice(1).toLowerCase();
+            computerSelection = computerPlay();
+            playRound(playerSelection, computerSelection);
+        }
+        else if (i > 5) {
+            break;
+        }
+
+        
+    }
+
+    if(compScore == playerScore){
+        alert("We both scored a total of " + `${compScore}` + " points. It's a tie!");
+    }
+    if(compScore < playerScore){
+        alert("I scored " + `${compScore}` + " points and you scored " + `${playerScore}` + " points. You win!");
+    }
+    if(compScore > playerScore){
+        alert("I scored " + `${compScore}` + " points and you scored " + `${playerScore}` + " points. I win!");
+    }
+}
+game()
+alert("Want to play again?");
+window.location.reload();
+
