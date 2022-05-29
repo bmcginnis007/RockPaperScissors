b90a62e113de81bbb3bdbb66ca0c280d46ef5c6b
Brian <technophile1974@gmail.com>
2022-05-29 05:29:35 -0400

    created buttons and put a background on my UI

--- rpsScript.js
+++ rpsScript.js
@@ -4,41 +4,69 @@ function computerPlay(){
     return random;
 }
 
-alert("Welcome to Rock Paper Scissors");
-alert("We will play 5 rounds in a game. Best of 5 wins. Good luck!");
-
-let compScore = 0;
-let playerScore = 0;
 
 
-let info = window.prompt("Choose Rock, Paper, or Scissors");
-playerSelection = info.charAt(0).toUpperCase() + info.slice(1).toLowerCase();
-computerSelection = computerPlay();
-function playRound(playerSelection, computerSelection){
+
+function options(){
+    
+    document.getElementById("rock").addEventListener("click", function(){
+         playerSelection = "Rock";
+         computerSelection = computerPlay();
+        playRound(computerSelection, playerSelection);
+        console.log(`${playerSelection}`);
+        console.log(`${computerSelection}`);
+        
+    });
+        
+    document.getElementById("paper").addEventListener("click", function(){
+         playerSelection = "Paper";
+         computerSelection = computerPlay();
+        playRound(computerSelection, playerSelection);
+        console.log(`${playerSelection}`);
+        console.log(`${computerSelection}`);
+    });
+    document.getElementById("scissors").addEventListener("click", function(){
+         playerSelection = "Scissors";
+         computerSelection = computerPlay();
+        playRound(computerSelection, playerSelection);
+        console.log(`${playerSelection}`);
+        console.log(`${computerSelection}`);
+        
+    });
+    }
+
+    let compScore = 0;
+    let playerScore = 0;
+
+let playerSelection = options();
+let computerSelection = computerPlay();
+
+function playRound(computerSelection, playerSelection){
+    
     if(computerSelection == playerSelection){
         return alert("It's a tie!");
     }
-    if(computerSelection == "Rock" && playerSelection == "Paper"){
+    else if(computerSelection == "Rock" && playerSelection == "Paper"){
         ++playerScore
         return alert("I chose Rock. You chose Paper. Paper covers Rock. You have " + `${playerScore}` + " point."); 
     }
-    if(computerSelection == "Rock" && playerSelection == "Scissors"){
+    else if(computerSelection == "Rock" && playerSelection == "Scissors"){
         ++compScore
         return alert("I chose Rock. You chose Scissors. Rock breaks Scissors. I have " + `${compScore}` + " point.");
     }
-    if(computerSelection == "Paper" && playerSelection == "Scissors"){
+    else if(computerSelection == "Paper" && playerSelection == "Scissors"){
         ++playerScore
         return alert("I chose Paper. You chose Scissors. Scissors cut Paper. You have " + `${playerScore}` + " point.");
     }
-    if(computerSelection == "Paper" && playerSelection == "Rock"){
+    else if (computerSelection == "Paper" && playerSelection == "Rock"){
         ++compScore
         return alert("I chose Paper. You chose Rock. Paper covers Rock. I have " +`${compScore}` + " point.");
     }
-    if(computerSelection == "Scissors" && playerSelection == "Rock"){
+    else if(computerSelection == "Scissors" && playerSelection == "Rock"){
         ++playerScore
         return alert("I chose Scissors. You chose Rock. Rock breaks Scissors. You have " +`${playerScore}` + " point.");
     }
-    if(computerSelection == "Scissors" && playerSelection == "Paper"){
+   else if(computerSelection == "Scissors" && playerSelection == "Paper"){
         ++compScore
         return alert("I chose Scissors. You chose Paper. Scissors cut Paper. I have " +`${compScore}` + " point.");
     }
@@ -47,9 +75,7 @@ function playRound(playerSelection, computerSelection){
 
 
 
-console.log(`You chose ${playerSelection}`);
-console.log(`I chose ${computerSelection}`);
-console.log(playRound(playerSelection, computerSelection));
+
 
 function game(){
     for (let i = 0; i < 5; i++) {
@@ -59,12 +85,11 @@ function game(){
             computerSelection = computerPlay();
             playRound(playerSelection, computerSelection);
         }
-        else if (i > 5) {
-            break;
-        }
+      
 
         
     }
+    
 
     if(compScore == playerScore){
         alert("We both scored a total of " + `${compScore}` + " points. It's a tie!");
@@ -76,7 +101,7 @@ function game(){
         alert("I scored " + `${compScore}` + " points and you scored " + `${playerScore}` + " points. I win!");
     }
 }
-game()
-alert("Want to play again?");
-window.location.reload();
+
+
+
 
