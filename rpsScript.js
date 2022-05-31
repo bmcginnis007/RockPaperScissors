96a9aab22c2c5d85a5c9d4b378281a225abd578b
Brian <technophile1974@gmail.com>
2022-05-31 04:19:01 -0400

    wrote a function to control scoreboard

--- rpsScript.js
+++ rpsScript.js
@@ -13,6 +13,28 @@ function options(){
          playerSelection = "Rock";
          computerSelection = computerPlay();
         playRound(computerSelection, playerSelection);
+        document.getElementById("td1").innerHTML = "X";
+        document.getElementById("td2").innerHTML = "";
+        document.getElementById("td3").innerHTML = "";
+       
+        if (computerSelection == "Scissors"){
+            document.getElementById("td3b").innerHTML = "X";
+            document.getElementById("td2b").innerHTML = "";
+            document.getElementById("td1b").innerHTML = "";
+        }
+        if (computerSelection == "Rock"){
+            document.getElementById("td1b").innerHTML = "X";
+            document.getElementById("td2b").innerHTML = "";
+            document.getElementById("td3b").innerHTML = "";
+        }
+        if (computerSelection == "Paper"){
+            document.getElementById("td2b").innerHTML = "X";
+            document.getElementById("td1b").innerHTML = "";
+            document.getElementById("td3b").innerHTML = "";
+        }
+
+        scoreboard();
+
         console.log(`${playerSelection}`);
         console.log(`${computerSelection}`);
         
@@ -22,7 +44,28 @@ function options(){
          playerSelection = "Paper";
          computerSelection = computerPlay();
         playRound(computerSelection, playerSelection);
-        document.getElementById("td1").innerHTML = "X";
+        document.getElementById("td2").innerHTML = "X";
+        document.getElementById("td1").innerHTML = "";
+        document.getElementById("td3").innerHTML = "";
+        
+        if (computerSelection == "Scissors"){
+            document.getElementById("td3b").innerHTML = "X";
+            document.getElementById("td2b").innerHTML = "";
+            document.getElementById("td1b").innerHTML = "";
+        }
+        if (computerSelection == "Rock"){
+            document.getElementById("td1b").innerHTML = "X";
+            document.getElementById("td2b").innerHTML = "";
+            document.getElementById("td3b").innerHTML = "";
+        }
+        if (computerSelection == "Paper"){
+            document.getElementById("td2b").innerHTML = "X";
+            document.getElementById("td1b").innerHTML = "";
+            document.getElementById("td3b").innerHTML = "";
+        }
+
+        scoreboard();
+
         console.log(`${playerSelection}`);
         console.log(`${computerSelection}`);
     });
@@ -30,6 +73,27 @@ function options(){
          playerSelection = "Scissors";
          computerSelection = computerPlay();
         playRound(computerSelection, playerSelection);
+        document.getElementById("td3").innerHTML = "X";
+        document.getElementById("td1").innerHTML = "";
+        document.getElementById("td2").innerHTML = "";
+        if (computerSelection == "Scissors"){
+            document.getElementById("td3b").innerHTML = "X";
+            document.getElementById("td2b").innerHTML = "";
+            document.getElementById("td1b").innerHTML = "";
+        }
+        if (computerSelection == "Rock"){
+            document.getElementById("td1b").innerHTML = "X";
+            document.getElementById("td2b").innerHTML = "";
+            document.getElementById("td3b").innerHTML = "";
+        }
+        if (computerSelection == "Paper"){
+            document.getElementById("td2b").innerHTML = "X";
+            document.getElementById("td1b").innerHTML = "";
+            document.getElementById("td3b").innerHTML = "";
+        }
+
+        scoreboard();
+
         console.log(`${playerSelection}`);
         console.log(`${computerSelection}`);
         
@@ -44,33 +108,78 @@ let computerSelection = computerPlay();
 
 function playRound(computerSelection, playerSelection){
     
-    if(computerSelection == playerSelection){
-        return alert("It's a tie!");
-    }
-    else if(computerSelection == "Rock" && playerSelection == "Paper"){
+    if(computerSelection == "Rock" && playerSelection == "Paper"){
         ++playerScore
-        return alert("I chose Rock. You chose Paper. Paper covers Rock. You have " + `${playerScore}` + " point."); 
+        document.getElementById("td2").style.backgroundColor = "green";
+        document.getElementById("td1b").style.backgroundColor = "red";
     }
+
     else if(computerSelection == "Rock" && playerSelection == "Scissors"){
         ++compScore
-        return alert("I chose Rock. You chose Scissors. Rock breaks Scissors. I have " + `${compScore}` + " point.");
+       
     }
     else if(computerSelection == "Paper" && playerSelection == "Scissors"){
         ++playerScore
-        return alert("I chose Paper. You chose Scissors. Scissors cut Paper. You have " + `${playerScore}` + " point.");
+     
     }
     else if (computerSelection == "Paper" && playerSelection == "Rock"){
         ++compScore
-        return alert("I chose Paper. You chose Rock. Paper covers Rock. I have " +`${compScore}` + " point.");
+  
     }
     else if(computerSelection == "Scissors" && playerSelection == "Rock"){
         ++playerScore
-        return alert("I chose Scissors. You chose Rock. Rock breaks Scissors. You have " +`${playerScore}` + " point.");
+       
     }
    else if(computerSelection == "Scissors" && playerSelection == "Paper"){
         ++compScore
-        return alert("I chose Scissors. You chose Paper. Scissors cut Paper. I have " +`${compScore}` + " point.");
+      
     }
+}
+
+function scoreboard(){
+    if(playerScore == 1){
+        document.getElementById("td4").innerHTML = "1";
+    } 
+
+    if(playerScore == 2){
+        document.getElementById("td4").innerHTML = "2";
+    }
+
+    if(playerScore == 3){
+        document.getElementById("td4").innerHTML = "3";
+    } 
+
+    if(playerScore == 4){
+        document.getElementById("td4").innerHTML = "4";
+    } 
+
+    if(playerScore == 5){
+        document.getElementById("td4").innerHTML = "5";
+        document.getElementById("td4").style.backgroundColor = "green";
+    } 
+
+    if(compScore == 1){
+        document.getElementById("td4b").innerHTML = "1";
+    }
+    
+    if(compScore == 2){
+        document.getElementById("td4b").innerHTML = "2";
+    } 
+
+    if(compScore == 3){
+        document.getElementById("td4b").innerHTML = "3";
+    } 
+
+    if(compScore == 4){
+        document.getElementById("td4b").innerHTML = "4";
+    } 
+
+    if(compScore == 5){
+        document.getElementById("td4b").innerHTML = "5";
+        document.getElementById("td4b").style.backgroundColor = "green";
+    } 
+
+
 }
 
 
